060fda151a69feb05e6350426761d666c09d6f25
shtanmoy <tanmoysharma46@gmail.com>
2023-09-22 21:51:02 +0600

    get input from command line node index.js add data.txt some text

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const http = require("http");
-const data = require("./data.js");
+const fs = require('fs');
+const input = process.argv;
 
-http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':"application\json"})
-    res.write(JSON.stringify(data))
-    res.end()
-}).listen(8000)
+if (input[2] === 'add') {
+    fs.writeFileSync(input[3], input[4])
+}
+else if (input[2] === 'remove') {
+    fs.unlinkSync(input[3])
+}
+else { console.log("invalid input") }
