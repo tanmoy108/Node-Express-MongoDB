8b87876532dfd5e8aa77770f5b833e2c47d86a85
shtanmoy <tanmoysharma46@gmail.com>
2023-09-22 20:51:29 +0600

    basic api with http

--- 02_createServer.js
+++
@@ -1,6 +0,0 @@
-const http = require("http");
-
-http.createServer((req,res)=>{
-    res.write("<h1> hello bro <h2/>")
-    res.end()
-}).listen(8000);
---
+++ data.js
@@ -0,0 +1,18 @@
+exports.data = [
+    {
+        name:"tanmoy",
+        email:"fdfjsdf@gmail.com"
+    },
+    {
+        name:"tanmoys",
+        email:"fdfj444f@gmail.com"
+    },
+    {
+        name:"tanmoyss",
+        email:"fd33df@gmail.com"
+    },
+    {
+        name:"tanmoyss",
+        email:"fdf@gmail.com"
+    }
+]
