7242f2b4cdccc27d44b2aea9d4b835a26b85b009
shtanmoy <tanmoysharma46@gmail.com>
2023-09-23 20:55:21 +0600

    ejs template,common header

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
-const fs = require("fs");
-const path = require("path");
-const dirPath = path.resolve(__dirname, "crud");
+const express =require("express");
+const server = express();
 
-//create -------------------
-fs.writeFileSync(`${dirPath}/data.txt`, "this is crud operation through fs module");
-const filePath = path.resolve(dirPath, "data.txt");
+server.set("view engine","ejs")
 
-//async read ----------------------------
-// fs.readFile(filePath,(err,item)=>{
-//     console.log(item) // buffer value
-// })
-
-fs.readFile(filePath, 'utf-8', (err, item) => {
-    console.log(item);
+server.get("/ejs",(_,res)=>{
+    const useInfo = {
+        name:"tanmoy",
+        email:"tanmooy@gmail.com",
+        skill:['js','react','node','c++']
+    }
+    res.render("new",{useInfo});
 })
 
-//async update text or add text --------------------
-setTimeout(() => {
-    fs.appendFile(filePath, " added later", (err) => {
-        if (!err) console.log("text updated")
-    })
-}, 5000)
-
-//rename text file
-setTimeout(() => {
-    fs.rename(filePath, `${dirPath}/reName.txt`, (err) => {
-        if (!err) console.log("rename successful")
-    })
-}, 7000)
-
-//delete file .........................
-setTimeout(() => {
-    fs.unlinkSync(`${dirPath}/reName.txt`)
-}, 9000)
+server.listen(8000,()=>{
+    console.log("server connected")
+})
